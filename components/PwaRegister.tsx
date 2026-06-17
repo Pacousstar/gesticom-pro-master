533cf6e4b7a6dcf896d970ab8f0b3a564566b7c1
GestiCom Production Audit <gsn-expertises@gesticom.pro>
2026-06-17 12:44:28 +0000

    Desactiver SW en dev (PwaRegister) + sw.js redirect follow

--- components/PwaRegister.tsx
+++ components/PwaRegister.tsx
@@ -4,6 +4,12 @@ import { useEffect } from 'react'
 
 export default function PwaRegister() {
   useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(r => r.forEach(s => s.unregister()))
+      }
+      return
+    }
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(() => {})
     }
